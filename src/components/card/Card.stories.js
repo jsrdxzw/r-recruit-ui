3f581cca2d0d2e4b2f59028c4663674f727b7eb6
xuzhiwei <jsrdxzw@163.com>
2019-04-20 09:44:41 +0900

    first commit

---
+++ src/components/card/Card.stories.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {storiesOf} from '@storybook/react';
+import Card from './index'
+
+
+storiesOf('Card', module)
+    .add('normal', () =>
+        <Card title={'Recruit card'} type={'normal'}>
+                <div>Im card in the body</div>
+        </Card>
+    )
+    .add('nested', () =>
+        <Card title={'Recruit card'} type={'nested'}>
+                <div>Im card in the body</div>
+        </Card>
+    )
---
+++ src/components/card/Card.tsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import cx from 'classnames'
+import './card.scss'
+
+export interface ICardProps {
+    type?: 'normal' | 'nested',
+    title?: React.ReactNode,
+    action?: React.ReactNode,
+    style?: React.CSSProperties, //React style object
+    bodyStyle?: React.CSSProperties,
+    loading?: boolean,
+    className?: string,
+    prefix?: string
+}
+
+export default class Card extends React.PureComponent<ICardProps> {
+    static defaultProps = {
+        type: 'normal',
+        style: {},
+        bodyStyle: {},
+        loading: false,
+        prefix: 'recruit'
+    };
+
+    render() {
+        const {
+            type,
+            title,
+            action,
+            loading,
+            style,
+            children,
+            className,
+            bodyStyle,
+            prefix,
+        } = this.props;
+        return (
+            <div
+                className={cx(`${prefix}-card`, className, {
+                    [`${prefix}-card--normal`]: type === 'normal',
+                    [`${prefix}-card--nested`]: type === 'nested',
+                })}
+                style={style}
+            >
+                <div className={`${prefix}-card-header`}>
+                    {title && <h3 className={`${prefix}-card-header__title`}>{title}</h3>}
+                    {action && <div className={`${prefix}-card-header__action`}>{action}</div>}
+                </div>
+                <div className={`${prefix}-card-body`} style={bodyStyle}>
+                    {children}
+                </div>
+            </div>
+        )
+    }
+}
